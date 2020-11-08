71d69a90675e9623961b73e1d784406f48e31b8b
harshi2008 <72973436+harshi2008@users.noreply.github.com>
2020-11-08 10:56:12 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,38 @@
+var fixedRect, movingRect;
+var gameobject1, gameobject2
+
+function setup() {
+  createCanvas(1200,800);
+  fixedRect = createSprite(600, 400, 50, 80);
+  fixedRect.shapeColor = "green";
+  fixedRect.debug = true;
+  movingRect = createSprite(400,200,80,30);
+  movingRect.shapeColor = "green";
+  movingRect.debug = true;
+  gameobject1 = createSprite(100, 200, 50, 50);
+  gameobject1.shapeColor = "green";
+  gameobject2 = createSprite(200, 200, 50, 50);
+  gameobject2.shapeColor = "green";
+
+}
+
+function draw() {
+  background(0,0,0);  
+  movingRect.x = World.mouseX;
+  movingRect.y = World.mouseY;
+
+  
+  if(isTouching(movingRect, gameobject1)){
+    movingRect.shapeColor = "red";
+    gameobject1.shapeColor = "red";
+  }
+  else{
+    movingRect.shapeColor = "green";
+    gameobject1.shapeColor = "green";
+    
+  }
+  
+  drawSprites();
+}
+
+
